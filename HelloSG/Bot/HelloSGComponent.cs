9b35a0021848db558748ba469087ff48dfb3ede6
dhananshan <navin.dhananshan@gmail.com>
2018-08-07 00:03:03 +0800

    added initial update of bot v4 version

---
+++ HelloSG/Bot/HelloSGComponent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HelloSG.Dialog;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+
+namespace HelloSG.Bot
+{
+    public class HelloSGComponent : IBot
+    {
+
+
+        private readonly DialogSet dialogs;
+
+        public HelloSGComponent()
+        {
+            // compose dialogs
+            dialogs = new DialogSet();
+            dialogs.Add("mainDialog", MainDialog.Instance);
+        }
+
+
+        /// <summary>
+        /// Every Conversation turn for our EchoBot will call this method. In here
+        /// the bot checks the Activty type to verify it's a message, bumps the 
+        /// turn conversation 'Turn' count, and then echoes the users typing
+        /// back to them. 
+        /// </summary>
+        /// <param name="context">Turn scoped context containing all the data needed
+        /// for processing this conversation turn. </param>        
+        public async Task OnTurn(ITurnContext context)
+        {
+
+            if (context.Activity.Type == ActivityTypes.Message)
+            {
+                var state = context.GetConversationState<Dictionary<string, object>>();
+                var dialogCtx = dialogs.CreateContext(context, state);
+
+                await dialogCtx.Continue();
+                if (!context.Responded)
+                {
+                    await dialogCtx.Begin("mainDialog");
+                }
+            }
+
+        }
+    }    
+}
---
+++ HelloSG/Dialog/MainDialog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Prompts.Choices;
+
+namespace HelloSG.Dialog
+{
+    public class MainDialog : DialogContainer
+    {
+        public MainDialog() : base(Id)
+        {
+            Dialogs.Add(DialogId, new WaterfallStep[]
+              {
+                    async (dc, args, next) =>
+                    {
+                        await dc.Prompt("choicePrompt", $"[MainDialog] I'm banking 🤖{Environment.NewLine}Would you like to check balance or make payment?",
+                            new ChoicePromptOptions
+                            {
+                                Choices = new[] {new Choice {Value = "Check balance"}, new Choice {Value = "Make payment"}}.ToList()
+                            });
+                    },
+                                        async (dc, args, next) =>
+                    {
+            dc.ActiveDialog.State = new Dictionary<string, object>(); // clear the dialog state 
+            await dc.End();
+                    }
+      });
+
+            Dialogs.Add("choicePrompt", new ChoicePrompt("en"));
+
+
+        }
+
+
+
+        public static string Id => "mainDialog";
+
+        public static MainDialog Instance { get; } = new MainDialog();
+    }
+}
+    
+
